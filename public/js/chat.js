2d73f455264b7febc9559a78bb62c59d0b4031b2
Khoa <52121010+11Khoa@users.noreply.github.com>
2020-06-08 20:54:05 +0700

    change

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,17 +8,19 @@ $(function(){
     var sent_name=$("#sent_name")
     var text_box=$("#text_box")
         // emit
-        sent_name.click(function(){
-            if(localStorage.getItem('name')){
-                user_name.attr('disabled','disabled');
-                user_name.val(localStorage.getItem('name'))
-                socket.emit("change_username",{user_name: user_name.val()})
-            }else{
+       
+        if(localStorage.getItem('name')){
+            user_name.attr('disabled','disabled');
+            user_name.val(localStorage.getItem('name'))
+            socket.emit("change_username",{user_name: user_name.val()})
+        }else{
+            sent_name.click(function(){
                 console.log(user_name.val());
                 socket.emit("change_username",{user_name: user_name.val()})
                 localStorage.setItem("name",user_name.val())
-            }
-        })
+                user_name.attr('disabled','disabled');
+            })
+        }
 
 
 
